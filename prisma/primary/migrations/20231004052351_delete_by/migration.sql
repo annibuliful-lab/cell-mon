67c876f78f736a94db8a9723975c22343f0f5213
annibuliful <lagmanmc@gmail.com>
2023-10-04 12:24:03 +0700

    feat: delete by

--- prisma/primary/migrations/20231004052351_delete_by/migration.sql
+++ prisma/primary/migrations/20231004052351_delete_by/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `updatedBy` on the `target` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
 ALTER TABLE "mission" ADD COLUMN     "deleteBy" TEXT;
 
@@ -14,8 +8,7 @@ ALTER TABLE "mission_target" ADD COLUMN     "deleteBy" TEXT;
 ALTER TABLE "phone_target" ADD COLUMN     "deleteBy" TEXT;
 
 -- AlterTable
-ALTER TABLE "target" DROP COLUMN "updatedBy",
-ADD COLUMN     "deleteBy" TEXT;
+ALTER TABLE "target" ADD COLUMN     "deleteBy" TEXT;
 
 -- AlterTable
 ALTER TABLE "target_evidence" ADD COLUMN     "deleteBy" TEXT;
