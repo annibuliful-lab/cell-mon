27e86831ec2953b2fe310fcb4d78cf415e9068e7
annibuliful <lagmanmc@gmail.com>
2023-11-13 19:01:57 +0700

    chore: pulumi infra

--- infrastructure/pulumi/src/constants.ts
+++ infrastructure/pulumi/src/constants.ts
@@ -3,3 +3,5 @@ export const TIME_ZONE = 'Asia/Bangkok';
 export const LABEL = 'cell-mon';
 export const TAG = 'dev';
 export const REGION = 'spg';
+export const K8S_VERSION = 'v1.28.3+2';
+export const K8S_NODE_QUANTITY = 1;
--- infrastructure/pulumi/src/database.ts
+++ infrastructure/pulumi/src/database.ts
@@ -1,28 +1,21 @@
 import * as vultr from '@ediri/vultr';
-import {
-  OBJECT_STORAGE_CLUSTER_ID,
-  LABEL,
-  TAG,
-  REGION,
-  TIME_ZONE,
-} from './constants';
-export const objectStorage = new vultr.ObjectStorage('cell-mon', {
-  clusterId: OBJECT_STORAGE_CLUSTER_ID,
-  label: LABEL,
-});
+import { TAG, REGION, TIME_ZONE } from './constants';
+import { vpc } from './vpc';
 
 export const pgDatabase = new vultr.Database('cell-mon-pg-dev', {
+  vpcId: vpc.id,
   clusterTimeZone: TIME_ZONE,
   databaseEngine: 'pg',
   databaseEngineVersion: '15',
   label: 'cell-mon',
-  plan: 'vultr-dbaas-hobbyist-cc-1-25-1',
+  plan: 'vultr-dbaas-hobbyist-cc-hp-amd-1-11-1',
   region: REGION,
   tag: TAG,
   password: 'cell-mon',
 });
 
 export const redisDatabase = new vultr.Database('cell-mon-redis-dev', {
+  vpcId: vpc.id,
   clusterTimeZone: TIME_ZONE,
   databaseEngine: 'redis',
   databaseEngineVersion: '7',
