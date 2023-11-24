43f1d1b156b7ac582af728d64a15c410e49c6563
annibuliful <lagmanmc@gmail.com>
2023-11-24 14:46:14 +0700

    chore: init

--- prisma/primary/migrations/20231124074407_phone_operator/migration.sql
+++ prisma/primary/migrations/20231124074407_phone_operator/migration.sql
@@ -2,7 +2,7 @@
   Warnings:
 
   - The primary key for the `phone_operator` table will be changed. If it partially fails, the table could be left without primary key constraint.
-  - A unique constraint covering the columns `[mnc,mcc,country,countryCode,brand,operator]` on the table `phone_operator` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[mnc,mcc]` on the table `phone_operator` will be added. If there are existing duplicate values, this will fail.
   - The required column `id` was added to the `phone_operator` table with a prisma-level default value. This is not possible if the table is not empty. Please add this column as optional, then populate it before making it required.
 
 */
@@ -12,7 +12,4 @@ ADD COLUMN     "id" UUID NOT NULL,
 ADD CONSTRAINT "phone_operator_pkey" PRIMARY KEY ("id");
 
 -- CreateIndex
-CREATE INDEX "phone_operator_mcc_mnc_idx" ON "phone_operator"("mcc", "mnc");
-
--- CreateIndex
-CREATE UNIQUE INDEX "phone_operator_mnc_mcc_country_countryCode_brand_operator_key" ON "phone_operator"("mnc", "mcc", "country", "countryCode", "brand", "operator");
+CREATE UNIQUE INDEX "phone_operator_mnc_mcc_key" ON "phone_operator"("mnc", "mcc");
